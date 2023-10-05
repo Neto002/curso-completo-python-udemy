79ef5c74b05adf383efb8496339567b68c805646
Neto002 <antoniogfneto92@hotmail.com>
2023-10-05 18:50:20 -0300

    count

---
+++ it_count.py
@@ -0,0 +1,14 @@
+from itertools import count
+
+c1 = count()
+r1 = range(10)
+
+print('c1', hasattr(c1, '__iter__'))
+print('c1', hasattr(c1, '__next__'))
+print('r1', hasattr(c1, '__iter__'))
+print('r1', hasattr(c1, '__next__'))
+
+for i in c1:
+    if i > 100:
+        break
+    print(i)
