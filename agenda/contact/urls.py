1e849eb74dbb9c25de2f48d0ad0467ac2dc616fb
Neto002 <antoniogfneto92@hotmail.com>
2023-11-03 15:17:56 -0300

    contact page

--- agenda/contact/urls.py
+++ agenda/contact/urls.py
@@ -5,5 +5,6 @@ from contact import views as contact_views
 app_name = 'contact'
 
 urlpatterns = [
+    path('<int:contact_id>/', contact_views.contact, name='contact'),
     path('', contact_views.index, name='index'),
 ]
--- agenda/contact/views/contact_views.py
+++ agenda/contact/views/contact_views.py
@@ -1,5 +1,5 @@
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.http import Http404, HttpRequest, HttpResponse
+from django.shortcuts import get_object_or_404, render
 
 from contact.models import Contact
 
@@ -7,10 +7,20 @@ from contact.models import Contact
 
 
 def index(request: HttpRequest) -> HttpResponse:
-    contacts = Contact.objects.all()
+    contacts = Contact.objects.filter(show=True).order_by('-id')
 
     context = {
         'contacts': contacts
     }
 
     return render(request, 'contact/index.html', context=context)
+
+
+def contact(request: HttpRequest, contact_id) -> HttpResponse:
+    single_contact = get_object_or_404(Contact, id=contact_id, show=True)
+
+    context = {
+        'contact': single_contact
+    }
+
+    return render(request, 'contact/contact.html', context=context)
